d66b14b06520ce9c9e696cd1cd14fbb7bca03c63
Deep learning by torch <44338800+yahan9416@users.noreply.github.com>
2018-12-28 22:06:44 +0800

    Update auto_encoder_essential_predicted.py

--- auto_encoder_essential_predicted.py
+++ auto_encoder_essential_predicted.py
@@ -1,5 +1,9 @@
-﻿#build a auto-encoder structure to 
-#构建去卷积的神经网络
+﻿# build a auto-encoder structure to predict the gene essential score
+# use the DepMap shRNA data as the train data, and the DepMap CRISPR data as the test data. 
+# the encode part we use the convolution structure and the decode part is the deconvolution part.
+# the input expression profile gene order is according Hierarchical clustering of TCGA gene profile 
+# the output gene order is according the Hierarchical clustering of shRNA and CRISPR value.
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -9,24 +13,25 @@ from torch.autograd import Variable
 import numpy as np
 import os
 
-
+# divide the data into expression and essential score
 def genTrainTest(dat,number_gene):
     N,L=dat.shape
     traindat=dat[0:N,0:number_gene] 
     trainlabel=dat[0:N,number_gene:L]
     return traindat,trainlabel
 
-#there are three CNN layer in encoder
-#no matter how much layers, when there is enough epochs ,the result is always same.
+# there are three CNN layer in encoder
+# in the convolution part this is only have one hide layer and one input/output layer
 class AtuoEncoder(nn.Module):
     def __init__(self):
         super(AtuoEncoder,self).__init__()
+        #the input in 1*13102 ,output is 8*935
         self.encod1=nn.Sequential(
             nn.Conv1d(in_channels=1,out_channels=8,kernel_size=13,stride=1),
             nn.Tanh(),
             nn.MaxPool1d(14,stride=14,return_indices=True)
             )
-        
+        #the input is 8*935, output is 16*66
         self.encod2=nn.Sequential(
             nn.Conv1d(8,16,kernel_size=12,stride=1),
             nn.Tanh(),
@@ -44,7 +49,7 @@ class AtuoEncoder(nn.Module):
             )
         
         
-
+# the input size x is equal to the size of output.
     def forward(self,x):
         #encoder part
         xnor=nn.BatchNorm1d(1,momentum=0.5)
@@ -82,7 +87,7 @@ number_gene=13102
 opt_Momentum = torch.optim.SGD(model.parameters(), lr=LR, momentum=0.5) 
 criterion=nn.MSELoss()
 
-
+#process the train and test data
 data=np.loadtxt("/Users/yahan/Desktop/predicted_gene_essential/gene_expressio_crispr_train_essential.txt",delimiter="\t")
 testdat,testlabel=genTrainTest(data,number_gene)
 data=np.loadtxt("/Users/yahan/Desktop/predicted_gene_essential/gene_expressio_shrna_test_essential.txt",delimiter="\t")
@@ -98,10 +103,12 @@ testlabel=Variable(torch.FloatTensor(testlabel))
 traindat=Variable(torch.FloatTensor(traindat))
 trainlabel=Variable(torch.FloatTensor(trainlabel))
 
+#because the CNN netowrk the input is three dimension
 traindat=traindat.view(431,1,13102)
 trainlabel=trainlabel.view(431,1,13102)
 testdat=testdat.view(289,1,13102)
 testlabel=testlabel.view(289,1,13102)
+
 for i in range(400):   
     outs=model(traindat)
     loss=criterion(outs,trainlabel)
